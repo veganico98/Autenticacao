8fbeaae6757377ed139fcdc7c016f2f94edeebb5
Vega <125098430+veganico98@users.noreply.github.com>
2024-06-04 22:03:40 -0300

    Add files via upload

---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,33 @@
+<?php
+
+    session_start();
+
+    $usuario_autenticado = false;
+
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abc'),
+    );
+
+    foreach($usuarios_app as $user) {
+
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){            
+            $usuario_autenticado = true;
+        }
+
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autenticado';
+        $_SESSION['autenticado'] = "SIM";
+    }else{
+        $_SESSION['autenticado'] = "NAO";
+        header('Location: index.php?login=erro');
+    }
+
+
+    // print_r($_GET);
+
+    // $_GET['email']
+    // $_GET['senha']
+?>
